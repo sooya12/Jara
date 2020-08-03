58de8ac0d5c9502d33b365823127bddb8da551b6
박진 <cent10@naver.com>
2020-08-03 14:44:06 +0900

    S03P13A308-18 | initial | 신고 기능 구현

---
+++ Project/backend/src/main/java/com/ssafy/jara/controller/ReportController.java
@@ -0,0 +1,34 @@
+package com.ssafy.jara.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.jara.dto.Report;
+import com.ssafy.jara.service.ReportService;
+
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+	
+	@Autowired
+	ReportService reportService;
+	
+	@ApiOperation(value = "사용자 신고 등록", response = String.class)
+	@PostMapping("/")
+	private ResponseEntity<String> insertBarter(@RequestBody Report report) {
+		if (reportService.insertReport(report) > 0) {
+			return new ResponseEntity<String>("success", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("fail", HttpStatus.BAD_REQUEST);
+		}
+	}
+}
---
+++ Project/backend/src/main/java/com/ssafy/jara/service/ReportService.java
@@ -0,0 +1,7 @@
+package com.ssafy.jara.service;
+
+import com.ssafy.jara.dto.Report;
+
+public interface ReportService {
+	public int insertReport(Report report);
+}
