447c2b048dcf49eed8e31f29c9e6711fb3ccd9e8
김영수 <suwon0521@naver.com>
2020-08-10 23:39:50 +0900

    S03P13A308-57 | initial | Barters 게시글 생성 페이지 구현

--- Project/frontend/src/views/Barters.vue
+++ Project/frontend/src/views/Barters.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid style="potition: relative">
+  <v-container fluid>
     <div class="mt-5">
       <div class="mx-3 font-weight-bold text-center text-sm-h3 text-h4">
         Barters
@@ -12,6 +12,56 @@
             class="col-12"
           >
           </v-text-field>
+        </v-flex>
+      </div>
+      <div class="mx-3">
+        <v-flex class="sm8 offset-sm3">
+          <v-toolbar flat color="white">
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs}">
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  dark
+                  class="my-1"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.contents" label="Contents"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.price" label="Price"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.tag_id" label="Type"></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
         </v-flex>
       </div>
       <!-- <div align="center" justify="center">
@@ -39,7 +89,7 @@
               :search="search"
               :custom-filter="filterSearch"
             >
-              <template v-slot:item.status="{ item }" v-slot:body.append>
+              <template v-slot:item.status="{ item }">
                 <v-chip :color="getColor(item.status)" dark>
                   {{ getStatus(item.status) }}
                 </v-chip>
@@ -58,7 +108,7 @@
 <script>
 import axios from 'axios'
 // import BartersItem from '../components/Barters/BartersItem.vue'
-// import _ from 'lodash'
+import _ from 'lodash'
 import { mapState } from 'vuex'
 
 export default {
@@ -80,12 +130,36 @@ export default {
       search: '',
       page: 1,
       pageCount: 0,
+      dialog: false,
+      editedIndex: -1,
+      editedItem: {
+        title: '',
+        tag_id: null,
+        contents: '',
+        writer: this.$store.state.userInfo.id,
+        price: 0,
+      },
+      defaultItem: {
+        title: '',
+        tag_id: null,
+        contents: '',
+        writer: this.$store.state.userInfo.id,
+        price: 0,
+      },
     }
   },
   computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
     ...mapState([
       'api_server'
     ])
+  },
+  watch: {
+    dialog(val) {
+      val || this.close()
+    }
   },
   created() {
     this.loading = true
@@ -116,6 +190,29 @@ export default {
     filterSearch(value, search) {
       return value != null && search !=null &&
         typeof value ==='string' && value.toString().indexOf(search) !== -1
+    },
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.barters[this.editedIndex], this.editedItem)
+      } else {
+        axios.post(`${this.$store.state.api_server}/barters/`,this.editedItem)
+          .then(res => {
+            // console.log(res.data)
+            this.barters.push(res.data)
+            this.barters = _.orderBy(this.barters,'id','desc')
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
+      this.close()
     }
   }
 }
