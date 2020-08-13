37bd344cdf8226b634b5cde3d5907ce495cd715b
박진 <cent10@naver.com>
2020-08-13 10:56:17 +0900

    update | 파일업로드 관련 불필요한 코드, 파일 삭제

--- Project/backend/src/main/java/com/ssafy/jara/controller/TipController.java
+++ Project/backend/src/main/java/com/ssafy/jara/controller/TipController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.jara.common.service.fileupload.FileUploadService;
 import com.ssafy.jara.dto.Tip;
 import com.ssafy.jara.service.TipCommentService;
 import com.ssafy.jara.service.TipService;
@@ -34,9 +33,6 @@ public class TipController {
 	
 	@Autowired
 	TipCommentService tipCommentService;
-	
-	@Autowired
-	FileUploadService fileUploadService;
 	
 	@ApiOperation(value = "팁 등록", response = String.class)
 	@PostMapping("")
@@ -100,7 +96,6 @@ public class TipController {
 		Tip tip = tipService.selectTip(id);
 		tip.setComments(tipCommentService.selectTipComments(id));
 		tip.setLikeAccounts(tipService.selectTipLikeAccounts(id));
-		tip.setStored_file_name(fileUploadService.selectBarterFileName(id));
 		
 		return new ResponseEntity<Tip>(tip, HttpStatus.OK);
 	}
