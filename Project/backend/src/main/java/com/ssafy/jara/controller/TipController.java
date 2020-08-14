061ca4e53cce0bdf7b41eed3451bb807b7863f7b
최현수 <hyeonsu2014@naver.com>
2020-08-14 13:47:57 +0900

    S03P13A308-60 | update | Tips 파일 업로드 시 기본 이미지 설정

--- Project/backend/src/main/java/com/ssafy/jara/controller/TipController.java
+++ Project/backend/src/main/java/com/ssafy/jara/controller/TipController.java
@@ -49,7 +49,28 @@ public class TipController {
 	private ResponseEntity<String> insertTipImg(@RequestBody Tip tip) {
 		HashMap<String, Object> hashMap = new HashMap<String, Object>();
 		hashMap.put("id", tip.getId());
-		hashMap.put("img_src", tip.getImg_src());
+		
+		String img_src = tip.getImg_src();
+		
+		// 작성자가 이미지를 등록하지 않은 경우, 기본 이미지 자라로 설정 
+		if(img_src.equals(null) || img_src == null || img_src.trim().equals("")) {
+			switch (tip.getTag_id()) { // 1 요리 2 세탁 3 청소 4 보관
+			case 1:
+				img_src = "https://firebasestorage.googleapis.com/v0/b/jara-8c5be.appspot.com/o/default%2Fyorijara.png?alt=media&token=99862c57-acdd-46f8-a3d5-992eab8582c8";
+				break;
+			case 2:
+				img_src = "https://firebasestorage.googleapis.com/v0/b/jara-8c5be.appspot.com/o/default%2Flaundryjara.png?alt=media&token=54fa2d72-dcec-4381-85c9-ffa6f5af4170";
+				break;
+			case 3:
+				img_src = "https://firebasestorage.googleapis.com/v0/b/jara-8c5be.appspot.com/o/default%2Fcleanjara.png?alt=media&token=2f6e3e49-f3a6-408a-b1f8-ae7dc6037595";
+				break;
+			case 4:
+				img_src = "https://firebasestorage.googleapis.com/v0/b/jara-8c5be.appspot.com/o/default%2Fbogwanjara.png?alt=media&token=5eeaa65e-c19c-4f9c-b256-96dec1b1f99e";
+				break;
+			}
+		}
+		
+		hashMap.put("img_src", img_src);
 		
 		if(tipService.updateTipImg(hashMap) > 0) {
 			return new ResponseEntity<String>("success", HttpStatus.OK);
