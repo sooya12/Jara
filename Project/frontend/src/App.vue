67a2dace0a96124d7356d8492abbb7ce6e508497
hesnying <hesnying@naver.com>
2020-08-14 23:52:19 +0900

    S03P13A308-97 | fix | 프로필 사진 업데이트 시 변경이 바로 안 이루어지는 부분  수정

--- Project/frontend/src/App.vue
+++ Project/frontend/src/App.vue
@@ -101,7 +101,7 @@
         >
           <v-list-item two-line>
             <v-list-item-avatar>
-              <v-icon v-if="!isLoggedIn||userInfo.img_src==null">mdi-account-circle</v-icon>
+              <v-icon v-if="!isLoggedIn||psas[userInfo.id]==null">mdi-account-circle</v-icon>
               <v-avatar v-else><img :src="psas[userInfo.id]"></v-avatar>
             </v-list-item-avatar>
             <v-list-item-content v-if="!isLoggedIn">
