f104cf22e63ebfa6f6bc5941ee6ae8d4407890d6
hesnying <hesnying@naver.com>
2020-08-18 18:59:15 +0900

    S03P13A308-15 | update | 수정

--- Project/frontend/src/components/Eithers/EitherDetail.vue
+++ Project/frontend/src/components/Eithers/EitherDetail.vue
@@ -305,7 +305,7 @@ export default {
       this.$router.push(`/accounts/${this.either.writer}`)
     },
     addComment() {
-      if (this.commentData.contents.trim().length <= 0 || this.commentData.choice == null) {
+      if (this.commentData.contents.trim().length <= 0) {
         alert('유효한 입력이 아닙니다.')
         this.commentData.contents = ''
         this.commentData.choice = null
