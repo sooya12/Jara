4952452c333f2f5776c8b3620ed37c64b5558707
박진 <cent10@naver.com>
2020-08-19 11:12:25 +0900

    S03P13A308-111 | update | Barter, BarterComment 주석 추가

--- Project/backend/src/main/java/com/ssafy/jara/service/BarterService.java
+++ Project/backend/src/main/java/com/ssafy/jara/service/BarterService.java
@@ -6,11 +6,24 @@ import java.util.List;
 import com.ssafy.jara.dto.Barter;
 
 public interface BarterService {
+	// 새로운 물물교환 등록
 	public int insertBarter(Barter barter);
+	
+	// 해당 물물교환 조회
 	public Barter selectBarter(int id);
+	
+	// 해당 물물교환 수정
 	public int updateBarter(Barter barter);
+	
+	// 해당 물물교환 삭제
 	public int deleteBarter(int id);
+	
+	// 전체 물물교환 조회
 	public List<Barter> selectListBarter();
+	
+	// 물물교환 상태 수정 (false(거래중)에서 true(거래완료)로 변경)
 	public int updateBarterStatus(HashMap<String, Object> hashMap);
+	
+	// 물물교환 게시글 이미지 경로 저장
 	public int updateBarterImgSrc(HashMap<String, Object> hashMap);
 }
